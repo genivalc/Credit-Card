4534f72c38e5e49b62ec8d5bcf70dc29932c5057
genivalc <genivalcandeiadon.neto@gmail.com>
2020-06-20 20:48:56 -0300

    Adicionando animação

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:CreditCard/controllers/offset_controller.dart';
 import 'package:CreditCard/controllers/page_controller.dart';
 import 'package:CreditCard/widgets/my_app_bar.dart';
 import 'package:CreditCard/widgets/panel_top.dart';
@@ -15,34 +16,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: ChangeNotifierProvider(
+              child: SafeArea(
           child: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          MyAppBar(),
-          PanelTop(),
-          PageView(
-            onPageChanged: (index) {
-              Provider.of<PageControllerApp>(context, listen:false ).setPageIndex(index);
-            },
-            controller: _pageController,
-            children: [
-              ItemPage(
-                imageURL:
-                    'https://us.123rf.com/450wm/grebeshkovmaxim/grebeshkovmaxim1805/grebeshkovmaxim180500202/100769321-colorful-smooth-gradient-color-background-design-for-your-project-design-.jpg?ver=6',
-              ),
-              ItemPage(
-                imageURL:
-                    'https://ak5.picdn.net/shutterstock/videos/1018020805/thumb/1.jpg',
-              ),
-              ItemPage(
-                imageURL:
-                    'https://i.pinimg.com/736x/5c/ee/af/5ceeafdc811d2269821fb03df29374ff.jpg',
-              ),
+            alignment: Alignment.topCenter,
+            children: <Widget>[
+              MyAppBar(),
+              PanelTop(),
+              PageView(
+                physics: ClampingScrollPhysics(),
+                onPageChanged: (index) {
+                  Provider.of<PageControllerApp>(context, listen: false).setPageIndex(index);
+                },
+                  controller: _pageController,
+                  children: <Widget>[
+                    ItemPage(index: 0,
+                      imageURL: 'https://us.123rf.com/450wm/grebeshkovmaxim/grebeshkovmaxim1805/grebeshkovmaxim180500202/100769321-colorful-smooth-gradient-color-background-design-for-your-project-design-.jpg?ver=6',),
+                    ItemPage(index: 1,
+                      imageURL: 'https://ak5.picdn.net/shutterstock/videos/1018020805/thumb/1.jpg',),
+                    ItemPage(index: 2,
+                      imageURL: 'https://i.pinimg.com/736x/5c/ee/af/5ceeafdc811d2269821fb03df29374ff.jpg',)
+                  ],
+                ),
             ],
           ),
-        ],
-      )),
+        ), create: (BuildContext context) => OffsetController(_pageController),
+      ),
     );
   }
 }
